3c407135aebe1514ca17662b703bf89655fbd8a2
JayHong53 <jiwoong.hong85@gmail.com>
2023-03-03 13:37:19 -0500

    first commit

---
+++ src/main/java/com/jiwoong/comp367/AppController.java
@@ -0,0 +1,22 @@
+package com.jiwoong.comp367;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@GetMapping("/")
+	public String getMain(Model model) {
+		
+		String title = "Welcome to COMP367";
+		String subTitle = "We learn Jenkins and it is fun!";
+		
+		model.addAttribute("title", title);
+		model.addAttribute("subTitle", subTitle);
+		
+		return "index";
+	}
+
+}
